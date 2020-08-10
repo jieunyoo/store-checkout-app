c328e53c0802c77810b8010f962f6a1f2df208a7
Jieun <jieunyoo201@gmail.com>
2020-08-10 08:59:43 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ContextProvider} from "./Context"
+
+
+
+ReactDOM.render(
+	<ContextProvider>
+  		<Router>
+    		<App />
+  		</Router>,
+	</ContextProvider>,
+  document.getElementById('root')
+);
+
